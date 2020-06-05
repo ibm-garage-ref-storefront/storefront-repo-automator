0cf4f62bc0670b12b25efa5affc6566b1bfc783c
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-06-05 14:57:24 -0500

    included README file on how to use the script

---
+++ README.md
@@ -0,0 +1,3 @@
+# How to run the script
+
+`initials=enter-your-initials ./run.sh`
--- run.sh
+++ run.sh
@@ -1,39 +1,34 @@
 org=storefront-dev
-initials=hk
-spring_repos=([1]="orders-ms-spring" [2]="auth-ms-spring" [3]="customer-ms-spring" [4]="catalog-ms-spring" [5]="inventory-ms-spring" [6]="storefront-ui", [7]="storefront-mobile-ionic")
+initials=${initials: hk}
+spring_repos=([1]="orders-ms-spring" [2]="auth-ms-spring" [3]="customer-ms-spring" [4]="catalog-ms-spring" [5]="inventory-ms-spring" [6]="storefront-ui" [7]="storefront-mobile-ionic")
 
 cd ..
 
 # create empty repo
 for repo in "${spring_repos[@]}"
 do 
-    echo "Creating repo $repo " 
-    echo -e "n" | gh repo create $org/$repo-$initials --public
+    echo "Creating repo " $org/$repo"-"$initials
+    echo -e "n" | gh repo create $org/$repo"-"$initials --public
 done
-
 sleep 5
 
 # clone repo
 for repo in "${spring_repos[@]}"
 do 
-    echo "Creating repo $repo " 
+    echo "Cloning repo ibm-garage-ref-storefront/" $repo
     gh repo clone https://github.com/ibm-garage-ref-storefront/$repo
 done
-
 sleep 5
 
 # Push all new repos to the org
 for repo in "${spring_repos[@]}"
 do 
-    echo "Pushing repo $repo "
-    pwd
-    echo "cding to the repo"
+    echo "Pushing repo " $org/$repo"-"$initials
     cd $repo
-    pwd
     rm -rf .git
     git init
     git add .
-    git remote add origin https://github.com/$org/$repo-$initials
+    git remote add origin https://github.com/$org/$repo"-"$initials
     git commit -m "forked $repo from storefront"
     git push -u origin master
     cd ../
