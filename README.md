5006eb661c0ea105cb4ff856165ebc70c3209b71
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-06-18 15:17:56 -0500

    updated README file and updated the run.sh script file to include additional edge cases

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-# How to run the script
+# Overview
+The responsibility of this repository is to dynamically generate a github organization,
+clone all of the repos' from ibm-garage-ref-storefront github and push them to the new organization that was generated.
 
-```bash
-export github_initials=your-initials
-export github_org=your-personal-org-name
-initials=$github_initials org=$github_org ./run.sh
-```
+- Create your Github Organization i.e `storefront-dev`
+
+- If you will be the only person in the new github organization, then you can run the following script:
+    ```bash
+    export org={your-github-org-you-created}
+    ./run.sh
+    ```
+
+- If you have multiple users using the same github organization, you can run the following command, to differentiate the storefront-ms from other users.
+
+    ```bash
+    export org={your-github-org-you-created}
+    export initials={your-initials}"-"
+    ./run.sh
+    ```
--- run.sh
+++ run.sh
@@ -1,35 +1,33 @@
-org=${storefront-dev: storefront-dev}
-initials=${initials: hk}
 spring_repos=([1]="orders-ms-spring" [2]="auth-ms-spring" [3]="customer-ms-spring" [4]="catalog-ms-spring" [5]="inventory-ms-spring" [6]="storefront-ui" [7]="storefront-mobile-ionic")
 
 cd ..
-
 # create empty repo
 for repo in "${spring_repos[@]}"
 do 
-    echo "Creating repo " $org/$repo"-"$initials
-    echo -e "n" | gh repo create $org/$repo"-"$initials --public
+    echo "Creating repo:" "$org"/"$initials""$repo"
+    pwd
+    echo -e "n" | gh repo create "$org"/"$initials""$repo" --public
 done
 sleep 5
 
 # clone repo
 for repo in "${spring_repos[@]}"
 do 
-    echo "Cloning repo ibm-garage-ref-storefront/" $repo
-    gh repo clone https://github.com/ibm-garage-ref-storefront/$repo
+    echo "Cloning repos from ibm-garage-ref-storefront/"$repo""
+    gh repo clone https://github.com/ibm-garage-ref-storefront/"$repo"
 done
 sleep 5
 
 # Push all new repos to the org
 for repo in "${spring_repos[@]}"
 do 
-    echo "Pushing repo " $org/$repo"-"$initials
+    echo "Pushing repo " "$org"/"$initials""$repo"
     cd $repo
     rm -rf .git
     git init
     git add .
-    git remote add origin https://github.com/$org/$repo"-"$initials
-    git commit -m "forked $repo from storefront"
+    git remote add origin https://github.com/"$org"/"$initials""$repo"
+    git commit -m "forked "$repo" from storefront"
     git push -u origin master
     cd ../
     pwd
